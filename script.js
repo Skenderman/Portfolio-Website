9bdef0fe18e06ab05e03a725bae903b4491bc027
Skenderman <brilantkrasniqi26@gmail.com>
2024-08-24 19:13:47 +0200

    added resume section

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ menu.addEventListener("click", () => {
   menu.classList.toggle("active");
   menuContent.classList.toggle("active");
 });
+// SPLIDE
+
+new Splide("#project-showcase", {
+  type: "loop",
+  drag: "snap",
+  autoHeight: true,
+  focus: "center",
+  width: "325px",
+  arrows: false,
+  autoplay: "pause",
+  intersection: {
+    inView: {
+      autoplay: true,
+    },
+  },
+}).mount(window.splide.Extensions);
